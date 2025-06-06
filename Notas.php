c015b61f98e946d44a11d632cf10dfcf9a276f87
Mnlx309 <matheusnobrelopes@hotmail.com>
2025-06-05 22:16:26 -0300

    Aula 5

--- Notas.php
+++ Notas.php
@@ -23,25 +23,23 @@ $alnote =
 
 while (true)
 {
-    echo "1. Adicionar Aluno/nota\n2. Listar Alunos/Notas\n3.Calcular Média dos Alunos\n4. Sair\n";
+    echo "1. Adicionar Aluno/nota\n2. Listar Alunos/Notas\n3.Calcular Média dos Alunos\n4. Sair\n5. Média Geral\n";
     $menu = readline ("Escolha a Opção desejada: ");
-
     if($menu==1)
         {
             $aluno = readline ("Informe o Nome do Aluno a ser adicionado: ");
-            $nota = readline ("Informe a nota a ser adicionada: ");
-                if(array_key_exists($aluno, $alnote))
-                {
-                    $aluno[$alnote]+=$nota;
-                    echo "Nota Adicionada com Sucesso!";
-                }
-                else
-                {
-                
-                    $alnote[$aluno] = array($nota);
-                    echo "Aluno e Nota adicionados com Sucesso!\n";
-                    print_r($alnote);
-                }
+            $notas = [];
+              for ($i=1; $i<=3; $i++)
+              {
+                 $nota = readline ("Informe a nota a ser adicionada: ");
+                 array_push($notas,$nota);
+              }
+              $alnote[] = [
+                  "nome" => $aluno,
+                  "notas" => $notas
+                ];
+                print_r($alnote);
+              
         }
 
     if ($menu==2)
@@ -49,7 +47,7 @@ while (true)
                 foreach($alnote as $nota)
                     {
                         $nome = $nota["nome"];
-                        $notas = implode(",", $nota["notas"]);                      
+                        $notas = implode(",",$nota["notas"]);                      
                         echo "{$nome} : {$notas}\n";                   
                     }
         }
@@ -71,6 +69,20 @@ while (true)
             echo "saindo!";
             exit;
         }
+    if($menu==5)
+        {
+             $somat = 0;
+             $ame = 0;
+    foreach($alnote as $nota)
+                    {
+                       // $nome = $nota["nome"];
+                        $notas = $nota["notas"];
+                        $somat += array_sum($notas);
+                        $ame += count($notas);
 
+                     }
+        $mediag = $somat / $ame;
+        echo "A média Geral da Turma é: $mediag \n";
+                    }
 }
 ?>
